3a7e7d98f88bd52df3b612e0c4868b4f7bef4ad5
Vue Lee <vuelee09@gmail.com>
2022-07-23 13:31:41 -0500

    add the header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import About from './components/About';
+import Header from './components/Header';
 
 function App() {
     return (
         // the below is a language called JSX that can represent HTML in JavaScript
         // Think of functions that return JSX as functions that use document.createElement(JSX)
         <div>
+            <Header></Header>
             <main>
                 <About></About>
             </main>
---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Header() {
+        
+    return (
+        <header>
+            <h1>
+                <a href="/">Vue Lee</a>
+            </h1>
+        </header>
+    )
+}
+
+export default Header;
