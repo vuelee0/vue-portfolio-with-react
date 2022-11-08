050db6928373d55b718102ed454476313875c0c1
Vue Lee <vuelee09@gmail.com>
2022-11-07 22:50:02 -0600

    add skills and resume pdf

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -11,22 +11,37 @@ function Resume() {
                     <br />
                     <h3>Front End</h3>
                     <ul>
-                        <li>HTML</li>
-                        <li>CSS</li>
                         <li>JavaScript</li>
                         <li>React</li>
+                        <li>jQuery</li>
+                        <li>Handlebars.js</li>
+                        <li>Progressive Web Applications</li>
+                        <li>HTML</li>
+                        <li>CSS</li>
+                        <li>BootStrap</li>
                     </ul>
                     <br />
                     <h3>Back End</h3>
                     <ul>
-                        <li>ExpressJS</li>
-                        <li>NodeJS</li>
+                        <li>Model View Controller</li>
+                        <li>Express.js</li>
+                        <li>Node.js</li>
+                        <li>MySQL</li>
+                        <li>Sequelize</li>
+                        <li>Apollo</li>
+                        <li>GraphQL</li>
+                        <li>MongoDB</li>
+                        <li>Mongoose</li>
+                        <li>Heroku</li>
+                        <li>MongoDB Atlas</li>
+                        <li>Object Orietned Programming</li>
                     </ul>
                     <br />
-                    <h3>Databases</h3>
+                    <h3>Collaboration</h3>
                     <ul>
-                        <li>MySQL</li>
-                        <li>MongoDB</li>
+                        <li>Agile</li>
+                        <li>GitHub</li>
+                        <li>GitLab</li>
                     </ul>
                     <br />
                     <div>
